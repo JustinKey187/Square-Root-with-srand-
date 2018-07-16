1fe56d4ba39d05fe4ca3af914275727206cab239
Jmoney187 <justin.key@outlook.com>
2018-07-16 15:39:23 -0700

    fun with random numbers

--- Square rt/Source.cpp
+++ Square rt/Source.cpp
@@ -6,16 +6,18 @@ using namespace std;
 
 int main()
 {
+	int sqr;
+	std:cin >> sqr;
+
 	srand((unsigned int)time(NULL));
-
-	for (int x = 1; x <= 100; x++)
+	for (int x = 1; x <= 100; x++) // Produces up to 100 random numebers
 	{
-		cout << (rand() % 9) << endl;
+		cout << (rand() % 9) << endl; // In this example, it's asking the square root of 9
 
-		if (x * x == 9) {
+		if (x * x == 9) { // The sequence in the generated numbers gets flagged if it is the square root
 			cout << "YES" << endl;
 		}
 		else;
-		cout << "NOPE" << endl;
+		cout << "NOPE" << endl; // The app will spit out NOPE for any generation that isnt the square root of the number provided above
 	}
 }
